fcf21b87f6f2e55421c12f791d7161937520a6aa
Hyon Lee <hyonmail@gmail.com>
2014-08-01 12:01:02 -0700

    Birth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wuthlesscoin::Application.config.secret_token = '1da797505b67d6dac7099b5c00d729b7404744621d32f6ccd788259ba48bd0dd225ecaaeaa9979ed8609cd9073e6896b304f8bbdeb0d5b48344d820d9fe94bbb'
